df12770d9107a1ee5bd76a2adcd591f930674f5f
Purvesh Patil <95206214+Purveshhpatil@users.noreply.github.com>
2025-01-21 18:51:33 +0530

    Create about-us.php

---
+++ src/about-us.php
@@ -0,0 +1,14 @@
+<?php
+    include('connection.php');
+    session_start();
+?>
+<html>
+    <head>
+    <title align="center">About Us</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <style type="text/css"></style>
+      <body>
+    
+             
+</body>
+</html>
